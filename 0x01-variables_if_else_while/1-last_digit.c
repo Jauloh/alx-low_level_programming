0539ed1eafb71045eab746409b88046a10652f19
Jauloh <jeremie.zadi@uvci.edu.ci>
2022-03-11 20:03:29 -0800

    c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	i = n % 10;
 	if (i > 5)
-		printf("%s %d is %d and is  greater than 5\n", world, n, i);
+		printf("%s %d is %d and is greater than 5\n", world, n, i);
 	if (i == 0)
 		printf("%s %d is %d and is 0\n", world, n, i);
 	if (i < 6)
 		printf("%s %d is %d and is less than 6 and not 0\n", world, n, i);
 	return (0);
-}	
+}
