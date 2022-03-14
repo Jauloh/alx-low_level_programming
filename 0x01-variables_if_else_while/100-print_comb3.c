5935a8495cf647a915bf8398fe8724b327b06efc
Jauloh <jeremie.zadi@uvci.edu.ci>
2022-03-13 19:09:03 -0700

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - prints all possible different combination of two digits
+*
+* Return: Always 0 (succes)
+*/
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 90; d++)
+	{
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
