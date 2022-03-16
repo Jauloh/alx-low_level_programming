3ab25897d06473c30426d61b6b8d964e9894026f
Jauloh <jeremie.zadi@uvci.edu.ci>
2022-03-16 16:39:03 -0700

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Write a function that prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - Write a function that prints 10 times the alphabe
  * followed by a new line
  * Return: Any thing
  */
@@ -15,6 +15,7 @@ void print_alphabet_x10(void)
 			_putchar(n);
 		}
 		_putchar('\n');
+
 		i++;
 	}
 }
