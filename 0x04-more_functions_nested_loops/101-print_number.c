68b661ec8439e91ceab43a23b97f3f407b8defd8
Jauloh <jeremie.zadi@uvci.edu.ci>
2022-03-19 10:42:41 -0700

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,6 @@
  */
 void print_number()
 {
-	_putchar(98);
-	_putchar('\n');
-	_putchar(402);
-	_putchar('\n');
-	_putchar(1024);
-	_putchar('\n');
-	_putchar(0);
-	_putchar('\n');
-	_putchar(-98);
+	_putchar(n);
 	_putchar('\n');
 }
